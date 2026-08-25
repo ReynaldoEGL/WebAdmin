d51560d356e1549d20eb3277408e43c03e607162
ReynaldoEGL <emmanuelrgl1534@gmail.com>
2026-08-25 10:42:59 -0600

    Aplicado estilos al login

--- login.php
+++ login.php
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="css/pagina.css">
     <title>WebAdmin - Iniciar sesión</title>
 </head>
 
